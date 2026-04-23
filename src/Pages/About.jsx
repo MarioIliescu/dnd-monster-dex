46fd25ab16ac657ce7ae97f614c600f4cd4ba94d
Snopeczek <bsnopeczek@interia.pl>
2026-04-23 15:31:56 +0200

    Changing About page to fit better

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,6 +3,26 @@ export default function About(){
 
     return(
 
-    <p>Bartosz lubi stopy: metali, procentowe oraz femboyów.</p>
-    )
+    <p>A table top board game where two types of people play. The dungeon master and the players.
+
+        The dungeon master acts as the world, giving players descriptions and info about areas, roleplaying as the worlds inhabitants and reacting to what the players do. Sort of like the computer for a video game. The dungeon master weaves the story for the players to play in.
+
+        The players are the protagonist of the game, they go around interacting with everything through dialogue, puzzle solving, exploration, combat, etc. They can do as they like in the world and its the DMs job to react to what they do.
+
+        Its a back and forth game. The dungeon master says whats going on in the world, like in this town there's a murderer on the loose. And the players head out to deal with whatever's going on, like maybe apprehending the murderer or conspiring with them to murder even more people.
+        <br>
+        </br>
+        Monsters in D&D are any creature—ranging from animals to supernatural horrors—that serve as obstacles, antagonists, or threats for player characters. They possess stat blocks, are classified by type (e.g., Fiend, Undead) and size, and often feature in the Monster Manual. Common synonyms include creatures, adversaries, beasts, and NPCs (non-player characters).
+        Usage Examples and Categories
+        <ul>
+        Monsters are used by Dungeon Masters to create challenges, battles, and narrative encounters. They are generally categorized into 14 types:
+            <li>Aberrations: Alien beings like Beholders or Mind Flayers.</li>
+            <li>Beasts: Natural creatures, such as wolves or giant animals.</li>
+            <li>Fiends: Creatures from Lower Planes like Demons and Devils.</li>
+            <li>Monstrosities: Unnatural creatures like Mimics or Owlbears.</li>
+            <li>Undead: Reanimated corpses or spirits, such as Vampires or Zombies.</li>
+            <li>Others: Celestials, Constructs, Dragons, Elementals, Fey, Giants, Humanoids, Oozes, and Plants.</li>
+        </ul>
+    </p>
+ )
 }
